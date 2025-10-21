04a66491a93cc9590c1fdd8aa2e9db60c2c9fd9a
Nargiza <rustamxonovanargiza2005@gmail.com>
2025-10-21 09:08:07 +0500

    vercel.json added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'http';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedServer: Server;
 
-  app.enableCors({
-    origin: 'http://localhost:5173', // Frontend manzili
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // agar cookie/token bilan ishlatsang
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  });
+export default async function handler(req: any, res: any) {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+    app.enableCors({
+      origin: ['http://localhost:5173', 'https://your-frontend-domain.vercel.app'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    });
+
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+
+  cachedServer(req, res);
 }
-bootstrap();
